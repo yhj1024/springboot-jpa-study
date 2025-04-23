7d7da4e8b04d0fff493382c3a8a734ffbd3e94a8
hyojun9292 <hyojun99222@gmail.com>
2025-04-23 23:48:51 +0900

    디펜던시 분석

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -13,6 +13,10 @@ public class JpashopApplication {
 //		String data = hello.getData();
 //		System.out.println(data);
 
+//		디펜던시 분석
+//		핵심라이브러리 : 스프링 mvc, orm, jpa, 하이버네이트
+//		기타 : h2 db client, hikari cp, web(타임리프), 로깅 slf4j (인터페이스) log4j (구현체), 테스트
+//      mockito (목 lib), assertj (테스트 코드짧게 도와주는 라이브러리), spring-test (스프링 통합 테스트 지원)
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
